709ed978d306257b6415214ac9ac13c875d03b41
Yu Yasuda <yupianon@gmail.com>
2021-07-08 22:37:57 +0900

    言語スキルセットのPieチャートの作成と、表示に伴うレイアウトの調整をしました。

--- components/contents/Computer.js
+++ components/contents/Computer.js
@@ -1,6 +1,6 @@
-import { Radar } from "react-chartjs-2";
+import { Radar, Pie } from "react-chartjs-2";
 
-const data = {
+const totalSkillData = {
   labels: ["コンピュータ基礎", "ビジネス", "アプリ", "UI/UX", "インフラ", "AI"],
   datasets: [
     {
@@ -13,14 +13,14 @@ const data = {
     {
       label: "目標",
       data: [5, 6, 8, 7, 4, 4],
-      backgroundColor: "rgba(92, 255, 132, 0.2)",
-      borderColor: "rgba(99, 255, 132, 1)",
+      backgroundColor: "rgba(75, 192, 192, 0.2)",
+      borderColor: "rgba(75, 192, 192, 1)",
       borderWidth: 1,
     },
   ],
 };
 
-const options = {
+const raderOptions = {
   scales: {
     r: {
       beginAtZero: true,
@@ -28,6 +28,40 @@ const options = {
       max: 10,
     },
   },
+  plugins: {
+    legend: {
+      labels: {
+        padding: 30,
+      },
+      position: "bottom",
+    },
+  },
+};
+
+const langData = {
+  labels: ["JavaScript", "Python", "Java", "その他"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [60, 20, 15, 5],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const langOptions = {
   plugins: {
     legend: {
       labels: {
@@ -46,11 +80,11 @@ const Computer = () => {
       </div>
       <div className="md:grid md:grid-cols-5 md:gap-8">
         <div className="hidden md:block md:my-8 md:col-span-2">
-          <Radar data={data} options={options} />
+          <Radar data={totalSkillData} options={raderOptions} />
         </div>
         <div className="md:col-span-3">
           <div className="my-8 mx-4">
-            <div className="py-4">
+            <div className="py-4 text-xl">
               <p>これまでの経験</p>
             </div>
             <div className="py-4">
@@ -75,35 +109,41 @@ const Computer = () => {
               </ul>
             </div>
           </div>
-          <div className="my-8">
-            <div className="py-4">
-              <p>スキルセット</p>
-            </div>
-            <div className="my-8 md:hidden">
-              <Radar data={data} options={options} />
-            </div>
-            <div className="py-4">
-              <ul>
-                <li>
-                  <p>コンピュータ基礎</p>
-                </li>
-                <li>
-                  <p>ビジネス</p>
-                </li>
-                <li>
-                  <p>アプリケーション</p>
-                </li>
-                <li>
-                  <p>UI/UX</p>
-                </li>
-                <li>
-                  <p>インフラ</p>
-                </li>
-                <li>
-                  <p>AI</p>
-                </li>
-              </ul>
-            </div>
+        </div>
+        <div className="hidden md:block md:my-8 md:col-span-2">
+          <Pie data={langData} options={langOptions} />
+        </div>
+        <div className="my-8 md:col-span-3">
+          <div className="py-4 text-xl">
+            <p>スキルセット</p>
+          </div>
+          <div className="my-8 md:hidden">
+            <Radar data={totalSkillData} options={raderOptions} />
+          </div>
+          <div className="py-4">
+            <ul>
+              <li>
+                <p>コンピュータ基礎</p>
+              </li>
+              <li>
+                <p>ビジネス</p>
+              </li>
+              <li>
+                <p>アプリケーション</p>
+              </li>
+              <li>
+                <p>UI/UX</p>
+              </li>
+              <li>
+                <p>インフラ</p>
+              </li>
+              <li>
+                <p>AI</p>
+              </li>
+            </ul>
+          </div>
+          <div className="m-4 md:hidden">
+            <Pie data={langData} options={langOptions} />
           </div>
         </div>
       </div>
