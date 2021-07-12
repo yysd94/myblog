75bea058a8aa2a7dbf7efc7ff3a2a0424453b8e8
Yu Yasuda <yupianon@gmail.com>
2021-07-12 10:54:41 +0900

    経歴表示部のスタイリングを改善しました。

--- components/contents/CareerItem.js
+++ components/contents/CareerItem.js
@@ -3,15 +3,19 @@ const CareerItem = (props) => {
     const {itemKey, term, title, descriptions, visibleKey} = props;
     return (
         <div>
-            <div>{term}</div>
-            <div>{title}</div>
-            {itemKey === visibleKey && 
-                <div>
+            <div className="m-2 hover:bg-gray-100">
+                <div className="p-1">{term}</div>
+                <div className="p-1">{title}</div>
+            </div>
+          {itemKey === visibleKey && 
+            <div className="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+              <div className="">
                 {descriptions.map((description, index) => 
-                    <p key={index}>{description}</p>
+                    <p key={index} className="p-1 text-center">{description}</p>
                 )}
-                </div>
-            } 
+              </div>
+            </div>  
+          } 
         </div>
     )
 }
