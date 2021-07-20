26e9e5966ab143adcfa6771fcab169804fef7fbe
Yu Yasuda <yupianon@gmail.com>
2021-07-20 17:34:52 +0900

    socialpageのアイコン型リンクのサンプルを実装しました。

--- components/social/SocialLink.js
+++ components/social/SocialLink.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 const SocialLink = () => {
   return (
     <div>
-      <p>Sociallink</p>
+      <div className="text-center h-full flex flex-col justify-center rounded">
+        <div className="p-8">
+          <SocialIcon url="https://www.youtube.com/channel/UCXUGCY4Vnmjk5CoUcqUmCjA/featured" />
+        </div>
+        <div className="text-xl">
+          <p>description</p>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/social.js
+++ pages/social.js
@@ -1,8 +1,12 @@
 import Layout from "../components/Layout";
+import SocialLink from "../components/social/SocialLink";
 const Social = () => {
   return (
     <Layout title="Social">
-      <p className="text-4xl"> Social Links</p>
+      <div className="my-8 text-4xl">
+        <p> Social Links</p>
+      </div>
+      <SocialLink />
     </Layout>
   );
 };
