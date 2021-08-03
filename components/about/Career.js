031debc1999af6a9a8b79678bf630d4952e6b0e2
Yu Yasuda <yupianon@gmail.com>
2021-08-03 16:07:55 +0900

    経歴の詳細表示機能のシグニファイアを改善するため、カーソルが当たる部分にプルダウンアイコンを追加しました。

--- components/about/Career.js
+++ components/about/Career.js
@@ -21,6 +21,20 @@ const Career = () => {
               <div className="my-2 bg-gray-100">
                 <div className="p-1">{career.term}</div>
                 <div className="p-1">{career.title}</div>
+                <div className="flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
               </div>
               <div className="border border-gray-300 shadow rounded-md p-4 mx-auto">
                 <div className="">
@@ -36,6 +50,20 @@ const Career = () => {
             <div className="my-2 hover:bg-gray-100">
               <div className="p-1">{career.term}</div>
               <div className="p-1">{career.title}</div>
+              <div className="flex justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
             </div>
           )}
         </div>
