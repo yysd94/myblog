0d800163dcdb471278ed3c837fa745a12fccffc0
Yu Yasuda <yupianon@gmail.com>
2021-08-24 10:52:10 +0900

    ##

--- pages/about/engineering.js
+++ pages/about/engineering.js
@@ -18,55 +18,69 @@ const Engineering = () => {
           <p className="text-4xl">About Me</p>
         </div>
         <div className="my-4 py-4 w-screen flex flex-col place-content-between">
-          <div className="md:mx-2">
-            <div className="my-16">
-              <p className="text-2xl">Engineering</p>
-            </div>
-            <div className="md:flex md:justify-center md:px-16 lg:px-32 xl:px-48">
-              <div className="md:w-screen md:grid md:grid-cols-5 md:gap-4">
-                <div className="hidden chart-container md:block md:my-4 md:col-span-2">
-                  <div className="my-4 xl:pl-32">
-                    <div className="py-4 xl:text-lg">
-                      <p>得意な分野</p>
-                    </div>
+          {/* ページタイトル */}
+          <div className="my-16">
+            <p className="text-2xl">Engineering</p>
+          </div>
+
+          {/* コンテンツ */}
+          <div className="flex flex-col justify-center items-center md:px-16">
+            <div className="m-4 md:w-full md:flex md:justify-center">
+              {/* 大画面の場合、レーダーチャートを左に、キャリアを右に表示するようにする。 */}
+              <div className="hidden chart-container md:block md:w-1/2 md:px-8">
+                <div className="my-4 xl:pl-32">
+                  <div className="py-4 xl:text-lg">
+                    <p>得意な分野</p>
+                  </div>
+                  <div>
                     <Radar data={totalSkillData} options={raderOptions} />
                   </div>
-                  <div className="my-4 pt-2 xl:pl-32">
-                    <div className="py-4 xl:text-lg">
-                      <p>得意な言語</p>
-                    </div>
-                    <Pie data={langData} options={langOptions} />
-                  </div>
                 </div>
-                <div className="m-4 md:col-span-3">
-                  <div className="py-4">
-                    <Career />
+                <div className="my-4 pt-2 xl:pl-32">
+                  <div className="py-4 xl:text-lg">
+                    <p>得意な言語</p>
                   </div>
-                </div>
-                <div className="hidden md:block md:pt-4 md:text-xl md:col-span-5">
-                  <p>スキルセット</p>
-                </div>
-                <div className="m-4 md:col-span-5">
-                  <div className="py-4 text-xl md:hidden">
-                    <p>スキルセット</p>
-                  </div>
-                  <div className="my-4 md:hidden">
-                    <Radar data={totalSkillData} options={raderOptions} />
-                  </div>
-                  <div className="py-4">
-                    <Skillset />
-                  </div>
-                  <div className="m-4 md:hidden">
-                    <div className="py-4">
-                      <p>得意な言語</p>
-                    </div>
+                  <div>
                     <Pie data={langData} options={langOptions} />
                   </div>
                 </div>
               </div>
+              <div className="md:w-1/2">
+                <div className="py-4 text-xl md:hidden">
+                  <p>経歴</p>
+                </div>
+                <div className="py-4">
+                  <Career />
+                </div>
+              </div>
+            </div>
+
+            <div className="m-4">
+              <div className="hidden md:block md:mt-8 md:text-xl">
+                <p className="py-8">経験した技術</p>
+              </div>
+              <div className="md:hidden">
+                <div className="py-4 text-xl">
+                  <p>経験した技術</p>
+                </div>
+                <div className="my-4">
+                  <Radar data={totalSkillData} options={raderOptions} />
+                </div>
+              </div>
+              <div className="py-4">
+                <Skillset />
+              </div>
+              <div className="md:hidden">
+                <div className="py-4">
+                  <p>得意な言語</p>
+                </div>
+                <Pie data={langData} options={langOptions} />
+              </div>
             </div>
           </div>
-          <div className="mt-8 pb-8">
+
+          {/* ページフッター */}
+          <div className="mt-16 pb-8">
             <Link href="/about">
               <a className="bg-gray-800 text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
                 戻る
